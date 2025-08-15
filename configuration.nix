f4b1ba142ebb305802ebc18f01c920592f264688
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-15 11:38:31 +1000

    feat: :wrench: add minimal nix os config

---
+++ configuration.nix
@@ -0,0 +1,33 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+}@args:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  services.openssh.enable = true;
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    # change this to your ssh key
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDVfzHbNxtB2c2qPKMXTYHnxDx4O+cJ1y2LqdOda1h2bKwmfaM81poecVluny4B21Zv3axNTnlMmBs0nucgTN8xcQgfM66Eg5dq2Hgu79mA6aKXt3AIx1sMPwOtjSA6vpnUCk+DcKX2HfTPdMLVC7XRHpMqcCfaemIq5fcdGfhPkLs0mLP7NoIcPeFSPTiNfVQ+/BE2gj7110L1DmS2Y6K+PFehxaiYUMeokqQyRk/KVgYoZi+6O70PIWsswLepQYUhmMAZfIJiN3NjpsM0AMWFLEpMDzgztCZoFOYaNuVPog/VQiHVwvDYKmwR99MoCAX/hHvfJN+rGcc2mAARtO2EWkB3hh0jm7Y0Ojgc2BQkbtJRBp8Z313/nWHkyEsXEGeIs4wHa7fWygmSw+/FNLkeB5OotJ1iCB19b6KLWNmbRI4ka06MY2MmAAeWVx4fLTemzN1rtt4xf6VzoKA3JSeWhbYpoRtQfPNH6+qocaXRuHF16U8ES2sRkSM+L2Ta4+s= ask.mrdgh2821@outlook.com"
+  ]
+  ++ (args.extraPublicKeys or [ ]); # this is used for unit-testing this module and can be removed if not needed
+
+  system.stateVersion = "24.05";
+}
