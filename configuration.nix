846d81907acdd595e0cb298fb212c014b5a4ab4f
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-16 10:54:06 +1000

    fix: :wrench: add empty devices

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@
   ];
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
+    devices = [ ];
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
