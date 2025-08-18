de0bdf44344b4402cbb19dad839d36ff2bb15e15
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-18 23:10:57 +1000

    fix: fix path

--- nix/hosts/home-lab/flake.nix
+++ nix/hosts/home-lab/flake.nix
@@ -45,7 +45,7 @@
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
-          ./configuration.nix
+          ./default.nix
           nixos-facter-modules.nixosModules.facter
           {
             config.facter.reportPath =
