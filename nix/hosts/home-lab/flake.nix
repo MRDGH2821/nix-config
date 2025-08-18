a8ccb9e7a2a79f3ea2e06cada00536c77c7f9a66
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-18 23:11:24 +1000

    feat(hosts/home-lab): :fire: remove uneeded config

--- nix/hosts/home-lab/flake.nix
+++ nix/hosts/home-lab/flake.nix
@@ -12,34 +12,6 @@
       ...
     }:
     {
-      # Use this for all other targets
-      # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
-      nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          ./hardware-configuration.nix
-        ];
-      };
-
-      # Slightly experimental: Like generic, but with nixos-facter (https://github.com/numtide/nixos-facter)
-      # nixos-anywhere --flake .#generic-nixos-facter --generate-hardware-config nixos-facter facter.json <hostname>
-      nixosConfigurations.generic-nixos-facter = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          nixos-facter-modules.nixosModules.facter
-          {
-            config.facter.reportPath =
-              if builtins.pathExists ./facter.json then
-                ./facter.json
-              else
-                throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
-          }
-        ];
-      };
 
       nixosConfigurations.home-lab = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
