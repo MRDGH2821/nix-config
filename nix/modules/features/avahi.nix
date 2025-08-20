4400ec77a6261b3d7e88789800acf450ce6576c3
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-20 18:54:31 +1000

    fix(features): :wrench: add additional options to make avahi work

--- nix/modules/features/avahi.nix
+++ nix/modules/features/avahi.nix
@@ -3,7 +3,14 @@
   services.avahi = {
     enable = true;
     openFirewall = true;
-
+    nssmdns = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    publish = {
+      enable = true;
+      addresses = true; # Broadcast IP addresses
+      workstation = true; # Advertise this machine as a workstation
+    };
   };
 
   environment.systemPackages = with pkgs; [
