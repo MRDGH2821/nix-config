18efce847a9ae74edb883dba213b402ad599e5db
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-20 14:00:32 +1000

    chore: :wrench: add outputs

--- flake.nix
+++ flake.nix
@@ -5,4 +5,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
   };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # Development shell
+      devShells.${system}.default = pkgs.mkShell {
+        shellHook = ''
+          echo "Welcome to the nix-config development environment!"
+        '';
+      };
+    };
 }
