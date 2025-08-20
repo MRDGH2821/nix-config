e2e78c963b1076047d79541b774c920c63022d51
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-20 11:42:17 +1000

    feat: :fire: remove blueprint

--- flake.nix
+++ flake.nix
@@ -4,18 +4,5 @@
   # Add all your dependencies here
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    blueprint.url = "github:numtide/blueprint";
-    blueprint.inputs.nixpkgs.follows = "nixpkgs";
   };
-
-  # Load the blueprint
-  outputs =
-    inputs:
-    inputs.blueprint {
-      inherit inputs;
-      prefix = "nix/";
-      systems = [
-        "x86_64-linux"
-      ];
-    };
 }
