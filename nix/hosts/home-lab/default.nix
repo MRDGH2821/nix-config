9897b0b0560a38f6d74a31b43f39faed06cb0474
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-20 19:14:45 +1000

    fix(hosts/home-lab): :ambulance: prevent checking of log rotate
    This is a temporary hack to allow nixos rebuilds to succeed

--- nix/hosts/home-lab/default.nix
+++ nix/hosts/home-lab/default.nix
@@ -5,6 +5,7 @@
     ../../modules/features/docker.nix
     ../../modules/features/ssh.nix
     ../../modules/features/system-packages.nix
+    ../../modules/fixes/log-rotation.nix
     ../../modules/services/stirling-pdf.nix
     ../../vars/default.nix
     ./configuration.nix
