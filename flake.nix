cb0e5d1185d0dd2bc8be99800a89dc4a92131f40
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-01 23:38:31 +1000

    feat: :wrench: convert into proper flake unit

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
-  description = "Simple flake with a devshell";
+  description = "NixOS Homelab Configuration with Development Environment";
 
-  # Add all your dependencies here
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
@@ -17,12 +16,51 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      # Import our custom modules
+      myLib = import ./nix/mylib/auto-import.nix { inherit (pkgs) lib; };
     in
     {
-      # Development shell
-      devShells.${system}.default = import ./devShell.nix {
-        inherit pkgs;
-        sopsModule = sops-nix.nixosModules.sops;
+      # Development shells
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nil # Nix Language Server
+            compose2nix
+            deno
+            sops # SOPS tool for encryption/decryption
+            age # Age encryption tool
+            ssh-to-age # Convert SSH keys to age keys
+            nixpkgs-fmt
+            nixpkgs-review
+          ];
+
+          shellHook = ''
+            echo "Welcome to the nix-config development environment!"
+            echo "Available tools: nil, compose2nix, deno, sops, age, ssh-to-age"
+          '';
+        };
+
+        # Alternative shell for CI/testing
+        ci = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nil
+            sops
+            age
+            ssh-to-age
+          ];
+        };
+      };
+
+      # Export our utility libraries for reuse
+      lib = myLib;
+
+      # Export modules for reuse in other flakes
+      nixosModules = {
+        homelab-features = ./nix/modules/features;
+        homelab-services = ./nix/modules/services;
+        homelab-fixes = ./nix/modules/fixes;
+        homelab-vars = ./nix/vars;
       };
     };
 }
