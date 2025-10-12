ba88e901620fab761bcab66ec81a50a1a60b4718
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-13 00:09:24 +1100

    feat(devshell): :package: use flake utils for flake config

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     alejandra.url = "github:kamadorueda/alejandra/4.0.0";
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -10,34 +11,31 @@
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     alejandra,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-
-    # Import our custom modules
-    myLib = import ./nix/mylib/auto-import.nix {inherit (pkgs) lib;};
-  in {
-    # Development shells
-    devShells.${system} = {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nil # Nix Language Server
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      myLib = import ./nix/mylib/auto-import.nix {inherit (pkgs) lib;};
+      alejandraPkg =
+        alejandra.packages.${system}.default or alejandra.defaultPackage.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nil
           compose2nix
           deno
-          sops # SOPS tool for encryption/decryption
-          age # Age encryption tool
-          ssh-to-age # Convert SSH keys to age keys
+          sops
+          age
+          ssh-to-age
           nixpkgs-fmt
           nixpkgs-review
-          alejandra.defaultPackage.${system}
+          alejandraPkg
         ];
-
         shellHook = ''
           echo "Welcome to the nix-config development environment!"
           echo "Available tools: nil, compose2nix, deno, sops, age, ssh-to-age"
         '';
       };
-    };
-  };
+    });
 }
