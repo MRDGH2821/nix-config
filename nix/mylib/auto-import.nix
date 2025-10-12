84ad05562bd4f61afb6fb16875977d721691d2b1
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-12 21:51:14 +1100

    refactor: :recycle: add isNixModule function to exclude sample and hidden files

--- nix/mylib/auto-import.nix
+++ nix/mylib/auto-import.nix
@@ -6,10 +6,23 @@
   autoImportModules =
     dir:
     let
-      # Function to check if a file is a .nix file and not default.nix
-      isNixModule = name: type: type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix";
+      # Function to check if a file is a .nix file and looks like a module
+      # Exclude default.nix, flake.nix, sample files and hidden files.
+      isNixModule =
+        name: type:
+        let
+          isNix = lib.hasSuffix ".nix" name;
+          isHidden = lib.hasPrefix "." name;
+          isSample = lib.hasSuffix ".sample.nix" name || lib.hasSuffix ".example.nix" name;
+        in
+        type == "regular"
+        && isNix
+        && !isHidden
+        && name != "default.nix"
+        && !isSample
+        && name != "flake.nix";
 
-      # Get all .nix files in the specified directory
+      # Get all matching .nix files in the specified directory
       nixFiles = lib.attrNames (lib.filterAttrs isNixModule (builtins.readDir dir));
 
       # Convert filenames to paths
