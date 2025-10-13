d56550c6e689c94d63c95a1eb373596ae33973d6
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-14 10:32:53 +1100

    chore(devshell): :fire: remove scalpel

--- flake.nix
+++ flake.nix
@@ -8,51 +8,39 @@
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    alejandra,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      myLib = import ./nix/mylib/auto-import.nix {inherit (pkgs) lib;};
-      alejandraPkg =
-        alejandra.packages.${system}.default or alejandra.defaultPackage.${system};
-      mk_scalpel = {
-        matchers,
-        source,
-        destination,
-        user ? null,
-        group ? null,
-        mode ? null,
-      }:
-        pkgs.callPackage ./nix/packages/scalpel.nix {
-          inherit matchers source destination user group mode;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      alejandra,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        alejandraPkg = alejandra.packages.${system}.default or alejandra.defaultPackage.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs self; [
+            nil
+            compose2nix
+            deno
+            sops
+            age
+            ssh-to-age
+            nixpkgs-fmt
+            nixpkgs-review
+            alejandraPkg
+            nixos-rebuild-ng
+            nixos-container
+          ];
+          shellHook = ''
+            echo "Welcome to the nix-config development environment!"
+            echo "Available tools: nil, compose2nix, deno, sops, age, ssh-to-age"
+          '';
         };
-
-      nixosModules.scalpel = import ./nix/modules/scalpel {inherit self;};
-      nixosModule = self.nixosModules.scalpel;
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs self; [
-          nil
-          compose2nix
-          deno
-          sops
-          age
-          ssh-to-age
-          nixpkgs-fmt
-          nixpkgs-review
-          alejandraPkg
-          nixos-rebuild-ng
-          self.nixosModules.scalpel
-          nixos-container
-        ];
-        shellHook = ''
-          echo "Welcome to the nix-config development environment!"
-          echo "Available tools: nil, compose2nix, deno, sops, age, ssh-to-age"
-        '';
-      };
-    });
+      }
+    );
 }
