054476ac1d79072be525599b13020f84d68867ed
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-13 18:43:37 +1100

    fix(nix-cmds): add --sudo flag to nixos-rebuild-ng for improved sudo handling

--- nix-cmds.sh
+++ nix-cmds.sh
@@ -86,7 +86,7 @@ nixos_rebuild() {
     if [[ "${HOST_IP}" == "localhost" ]]; then
         sudo nixos-rebuild switch --flake "${FLAKE}" --verbose
     else
-        nixos-rebuild-ng switch --flake "${FLAKE}" --ask-sudo-password --target-host "${HOST}" --build-host "${HOST}"  --verbose
+        nixos-rebuild-ng switch --flake "${FLAKE}" --sudo --ask-sudo-password --target-host "${HOST}" --build-host "${HOST}"  --verbose
     fi
 }
 
