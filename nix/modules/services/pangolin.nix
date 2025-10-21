61714a181b7243e362cfb6ff917fac6848c87718
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-22 10:53:14 +1100

    fix(services): :wrench: remove sops-nix templates and add back scalpel method

--- nix/modules/services/pangolin.nix
+++ nix/modules/services/pangolin.nix
@@ -5,12 +5,6 @@
 }: {
   environment.systemPackages = with pkgs; [fosrl-pangolin];
   boot.kernelModules = ["wireguard"];
-  sops.templates = {
-    smtpEmail = "${config.sops.placeholder.smtpEmail}";
-    letsEncryptEmail = "${config.sops.placeholder.letsEncryptEmail}";
-    baseDomain = "${config.sops.placeholder.baseDomain}";
-  };
-
   services.pangolin = {
     enable = true;
     openFirewall = true;
@@ -26,15 +20,15 @@
       };
       smtp_host = "smtp.gmail.com";
       smtp_port = 587;
-      smtp_user = config.sops.templates.smtpEmail;
+      smtp_user = "!!SMTP_EMAIL!!";
       flags = {
         require_email_verification = false;
         disable_signup_without_invite = true;
         disable_user_create_org = true;
       };
     };
-    letsEncryptEmail = config.sops.templates.letsEncryptEmail;
-    baseDomain = config.sops.templates.baseDomain;
+    letsEncryptEmail = "!!LETSENCRYPT_EMAIL!!";
+    baseDomain = "!!BASE_DOMAIN!!";
     environmentFile = config.sops.secrets.pangolin.path;
     dnsProvider = "duckdns";
   };
