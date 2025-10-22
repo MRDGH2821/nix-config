07488ce014f1689285407714bdbf53e068717626
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-22 16:25:17 +1100

    refactor(secrets): :recycle: split secrets

--- nix/modules/services/pangolin.nix
+++ nix/modules/services/pangolin.nix
@@ -18,17 +18,7 @@
           prefer_wildcard_cert = true;
         };
       };
-      smtp_host = "smtp.gmail.com";
-      smtp_port = 587;
-      smtp_user = "!!SMTP_EMAIL!!";
-      flags = {
-        require_email_verification = false;
-        disable_signup_without_invite = true;
-        disable_user_create_org = true;
-      };
     };
-    letsEncryptEmail = "!!LETSENCRYPT_EMAIL!!";
-    baseDomain = "!!BASE_DOMAIN!!";
     environmentFile = config.sops.secrets.pangolin.path;
     dnsProvider = "duckdns";
   };
