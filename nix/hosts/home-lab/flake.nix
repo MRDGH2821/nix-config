6b9a944523ec2d3293130f2a226f912ef9c271cd
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-23 23:55:40 +1100

    chore(scalpel): :fire: remove scalpel related code

--- nix/hosts/home-lab/flake.nix
+++ nix/hosts/home-lab/flake.nix
@@ -3,45 +3,16 @@
   inputs.sops-nix.url = "github:Mic92/sops-nix";
   inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   outputs = {
-    self,
     nixpkgs,
     sops-nix,
     ...
-  }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    mk_scalpel = {
-      matchers,
-      source,
-      destination,
-      user ? null,
-      group ? null,
-      mode ? null,
-    }:
-      pkgs.callPackage ./../../packages/scalpel.nix {
-        inherit matchers source destination user group mode;
-      };
-
-    nixosModules.scalpel = import ./../../modules/scalpel {inherit self;};
-    nixosModule = self.nixosModules.scalpel;
-    nixosConfigurations = let
-      base-lab = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          sops-nix.nixosModules.sops
-          ./default.nix
-        ];
-      };
-    in {
-      home-lab = base-lab.extendModules {
-        modules = [
-          self.nixosModules.scalpel
-          ./../../modules/scalpel/pangolin-scalpel.nix
-        ];
-        specialArgs = {
-          prev = base-lab;
-        };
-      };
+  }: {
+    nixosConfigurations.home-lab = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./default.nix
+        sops-nix.nixosModules.sops
+      ];
     };
   };
 }
