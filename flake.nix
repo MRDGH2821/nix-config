fa7e838d28f0a101783f87f6d298822636a14af9
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-24 11:53:45 +1100

    build(devshell): :wrench: remove flake utils & simplify the devshell config

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     alejandra.url = "github:kamadorueda/alejandra/4.0.0";
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -11,33 +10,29 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     alejandra,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {system = system;};
-        alejandraPkg = alejandra.packages.${system}.default or alejandra.defaultPackage.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            age
-            alejandraPkg
-            compose2nix
-            deno
-            git-agecrypt
-            nil
-            nixos-rebuild-ng
-            nixpkgs-fmt
-            nixpkgs-review
-            sops
-            ssh-to-age
-          ];
-          shellHook = ''
-            echo "Welcome to the nix-config development environment!"
-            echo "Available tools: nil, compose2nix, deno, sops, age, ssh-to-age"
-          '';
-        };
-      }
-    );
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        age
+        alejandra.packages.${system}.default
+        compose2nix
+        deno
+        git-agecrypt
+        nil
+        nixos-rebuild-ng
+        nixpkgs-fmt
+        nixpkgs-review
+        sops
+        ssh-to-age
+      ];
+      shellHook = ''
+        echo "Welcome to the nix-config development environment!"
+        echo "Available tools: nil, compose2nix, deno, sops, age, ssh-to-age"
+      '';
+    };
+  };
 }
