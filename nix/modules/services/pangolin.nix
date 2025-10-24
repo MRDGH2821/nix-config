299472964cabbc93b9af4c8c086a2412e5bf961f
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-24 13:31:07 +1100

    refactor(services): :wrench: move domain config to secrets

--- nix/modules/services/pangolin.nix
+++ nix/modules/services/pangolin.nix
@@ -12,12 +12,6 @@
       app = {
         save_logs = true;
       };
-      domains = {
-        domain1 = {
-          cert_resolver = "letsencrypt";
-          prefer_wildcard_cert = true;
-        };
-      };
     };
     environmentFile = config.sops.secrets.pangolin.path;
     dnsProvider = "duckdns";
