cc5dce1e8ebc4d288a689ba20da74fb05d39c06d
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-26 00:26:27 +1100

    feat: migrate to nginx
    Inspired by this - https://nohup.no/nixos-nixos/#security-considerations

--- nix/hosts/home-lab/acme.nix
+++ nix/hosts/home-lab/acme.nix
@@ -3,14 +3,14 @@
     acceptTerms = true;
     defaults = {
       email = config.vars.email;
-      group = config.services.caddy.group;
+      # group = config.services.caddy.group;
     };
     certs."${config.vars.baseDomain}" = {
-      group = config.services.caddy.group;
-      domain = "${config.vars.baseDomain}";
-      extraDomainNames = ["*.${config.vars.baseDomain}"];
-      dnsProvider = config.vars.dnsProvider;
-      dnsPropagationCheck = true;
+      # group = config.services.caddy.group;
+      # domain = "${config.vars.baseDomain}";
+      # extraDomainNames = ["*.${config.vars.baseDomain}"];
+      # dnsProvider = config.vars.dnsProvider;
+      # dnsPropagationCheck = true;
       environmentFile = config.sops.secrets.acme.path;
     };
   };
---
+++ nix/modules/services/nginx.nix
@@ -0,0 +1,34 @@
+{config, ...}: let
+  dmn = config.vars.baseDomain;
+in {
+  networking.firewall.allowedTCPPorts = [80 443];
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    enableHTTP2 = true;
+    virtualHosts."${dmn}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:3000";
+          proxyWebsockets = false;
+          extraConfig =
+            "proxy_ssl_server_name on;"
+            + "proxy_pass_header Authorization;";
+        };
+      };
+    };
+    virtualHosts."pdf.${dmn}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.stirling-pdf.environment.SERVER_PORT}";
+        };
+      };
+    };
+  };
+}
