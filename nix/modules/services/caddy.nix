742cbf1c9ccb66ecf7d218f37f810af44648f880
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-26 13:36:57 +1100

    feat(services): :fire: use barebones config for caddy

--- nix/modules/services/caddy.nix
+++ nix/modules/services/caddy.nix
@@ -1,6 +1,5 @@
 {config, ...}: let
   dmn = config.vars.baseDomain;
-  certloc = config.security.acme.certs."${dmn}".directory;
 in {
   networking.firewall.allowedTCPPorts = [80 443];
 
@@ -8,18 +7,11 @@ in {
     enable = true;
     virtualHosts."${dmn}".extraConfig = ''
       respond "OK"
-
-      tls ${certloc}/cert.pem ${certloc}/key.pem {
-        protocols tls1.3
-      }
     '';
 
     virtualHosts."pdf.${dmn}".extraConfig = ''
       reverse_proxy http://localhost:${toString config.services.stirling-pdf.environment.SERVER_PORT}
 
-      tls ${certloc}/cert.pem ${certloc}/key.pem {
-        protocols tls1.3
-      }
     '';
   };
 }
