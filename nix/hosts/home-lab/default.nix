6b00dfa0003d1aa8a62725dd3b51ac4dbfc2899e
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-26 15:22:44 +1100

    refactor(hosts/home-lab): :recycle: move modules into a sub folder for conveinence

--- nix/hosts/home-lab/default.nix
+++ nix/hosts/home-lab/default.nix
@@ -1,13 +1,12 @@
 {
   imports = [
-    ./secrets/agecrypt/duckdns-domain.nix
-    ./acme.nix
-    ./sops.nix
     ../../modules/features
     ../../modules/fixes
     ../../modules/services
-    ../../vars/default.nix
+    ../../vars
     ./configuration.nix
     ./hardware-configuration.nix
+    ./modules
+    ./secrets/agecrypt/duckdns-domain.nix
   ];
 }
