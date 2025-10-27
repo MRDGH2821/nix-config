951cf233307a0ccf81156ca5d7f0192f4070cae4
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-27 11:16:15 +1100

    fix(caddy): :truck: add missing TLS resolver configuration for virtual hosts

--- nix/modules/services/caddy.nix
+++ nix/modules/services/caddy.nix
@@ -7,11 +7,17 @@ in {
     enable = true;
     virtualHosts."${dmn}".extraConfig = ''
       respond "OK"
+
+      tls {
+          resolvers 192.168.1.1:53 1.1.1.1:53 8.8.8.8:53
+      }
     '';
 
     virtualHosts."pdf.${dmn}".extraConfig = ''
       reverse_proxy http://localhost:${toString config.services.stirling-pdf.environment.SERVER_PORT}
-
+      tls {
+          resolvers 192.168.1.1:53 1.1.1.1:53 8.8.8.8:53
+      }
     '';
   };
 }
