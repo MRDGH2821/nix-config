d63964ade41623426c8334106ce0a1eac8a5b1b6
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-27 12:16:50 +1100

    feat(secrets): :closed_lock_with_key: add smtp config

---
+++ nix/hosts/home-lab/secrets/agecrypt/smtp.nix
@@ -0,0 +1,9 @@
+age-encryption.org/v1
+-> X25519 Pc9n0HSVVvQtfNy8VS7J4P8eYhlW5sdQd6xetMos6Ws
+MfeJBqDPeMQ/oiBPoqGL57COIIUvlg7Jf0skJforzhA
+-> =O0Mhp-grease GV1!|O4 #C4jn"@
+gt9KMwSbrRk7jL2ZrjB+iO7AGRrnQg
+--- RLP5K/4a21L4qhuc6IjIHHZvyKkJZOmQWId10tbqkh8
+Üÿ,ùoªÉÝyúSWvÌ»„µ?ÚõSŸÿ1*ø³f©™ßY6°¯
+Tg A\)mêHÿô×vÞ–›oaýlQb±úŽÙ³C5 .¬¤T%+Æè·…Ìi÷RJ’ý¡ûâ±
+/5¤D«üf:y[§Ñ`v]xÝ†mwT	Ñe
--- nix/vars/networking.nix
+++ nix/vars/networking.nix
@@ -17,5 +17,24 @@
       default = "your-provider";
       description = "DNS provider name, from https://go-acme.github.io/lego/dns/index.html";
     };
+
+    smtp = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          host = lib.mkOption {
+            type = lib.types.str;
+            default = "smtp.example.com";
+          };
+          port = lib.mkOption {
+            type = lib.types.port;
+            default = 587;
+          };
+          email = lib.mkOption {
+            type = lib.types.str;
+            default = "user@example.com";
+          };
+        };
+      };
+    };
   };
 }
