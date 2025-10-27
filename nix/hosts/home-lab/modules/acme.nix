e68a1786089ba4307773d990563809d4541ea643
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-27 11:48:18 +1100

    fix(services): :wrench: provide certificates by acme path

--- nix/hosts/home-lab/modules/acme.nix
+++ nix/hosts/home-lab/modules/acme.nix
@@ -6,7 +6,7 @@
       dnsResolver = "1.1.1.1:53";
       dnsPropagationCheck = true;
       environmentFile = config.sops.secrets.acme.path;
-      # group = config.services.caddy.group;
+      group = config.services.caddy.group;
     };
     certs."${config.networking.baseDomain}" = {
       domain = "${config.networking.baseDomain}";
