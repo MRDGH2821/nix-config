b4c72507673f1ebbb37e25ad7428506d3ec601a3
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-30 10:44:01 +1100

    refactor(shell): :wrench: make the script inline

--- nix/modules/shell/zsh.nix
+++ nix/modules/shell/zsh.nix
@@ -1,4 +1,24 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  promptInitScript = pkgs.writeShellScript "zsh-prompt-init" ''
+    # Provide path to oh-my-posh installed themes
+    omp_themes_dir="${pkgs.oh-my-posh}/share/oh-my-posh/themes"
+
+    # Get a list of all JSON theme files in the directory
+    omp_themes=($(ls $omp_themes_dir/*.json))
+
+    # Get the number of JSON theme files
+    num_files=''${#omp_themes[@]}
+
+    # Generate a random index
+    random_index=$((RANDOM % num_files))
+
+    # Select a random theme
+    random_omp_theme=''${omp_themes[$random_index]}
+
+    # Initialize oh-my-posh with the random theme
+    eval "$(oh-my-posh init zsh --config $random_omp_theme)"
+  '';
+in {
   environment.systemPackages = [
     pkgs.zimfw
     pkgs.oh-my-posh
@@ -14,9 +34,6 @@
       "nomatch"
       "notify"
     ];
-    promptInit = ''
-      omp_themes_dir="${pkgs.oh-my-posh}/share/oh-my-posh/themes"
-      ${builtins.readFile ./zsh-prompt-init.sh}
-    '';
+    promptInit = builtins.readFile promptInitScript;
   };
 }
