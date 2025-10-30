2da7834bf7fa862b5738c894ace8a0e327f4529e
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-30 15:57:21 +1100

    feat(services): :wrench: add forgejo service

---
+++ nix/modules/services/forgejo.nix
@@ -0,0 +1,45 @@
+{config, ...}: {
+  services.forgejo = {
+    enable = true;
+    database = {
+      type = "postgres";
+      host = "localhost";
+      createDatabase = true;
+    };
+    dump = {
+      enable = true;
+      backupDir = "${config.persistent_storage}/forgejo-dumps";
+      type = "tar.xz";
+    };
+    lfs = {
+      enable = true;
+    };
+    settings = {
+      server = {
+        DISABLE_SSH = true;
+      };
+      openid = {
+        ENABLE_OPENID_SIGNIN = true;
+        ENABLE_OPENID_SIGNUP = true;
+      };
+      oauth2_client = {
+        REGISTER_EMAIL_CONFIRM = true;
+        OPENID_CONNECT_SCOPES = "profile email openid";
+        ENABLE_AUTO_REGISTRATION = true;
+        ACCOUNT_LINKING = "login";
+      };
+      service = {
+        REGISTER_EMAIL_CONFIRM = true;
+        ENABLE_NOTIFY_MAIL = true;
+      };
+      mailer = {
+        ENABLED = true;
+        PROTOCOL = "smtp";
+        SMTP_ADDR = config.networking.smtp.host;
+        SMTP_PORT = config.networking.smtp.port;
+        USER = config.networking.smtp.email;
+        PASSWD_URI = config.sops.secrets.smtpPassword.path;
+      };
+    };
+  };
+}
