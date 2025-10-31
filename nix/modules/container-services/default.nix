5e699e97cc1184ffac78235262b715691d9f377e
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-10-31 23:21:24 +1100

    fix(services): :wrench: implement a communication fix for docker containers
    https://github.com/NixOS/nixpkgs/issues/417641

--- nix/modules/container-services/default.nix
+++ nix/modules/container-services/default.nix
@@ -2,4 +2,13 @@
   autoImportLib = import ../../mylib/auto-import.nix {inherit lib;};
 in {
   imports = autoImportLib.autoImportFolders ./.;
+  networking.firewall = {
+    extraCommands = "
+      iptables -I nixos-fw 1 -i br+ -j ACCEPT
+    ";
+    extraStopCommands = "
+      iptables -D nixos-fw -i br+ -j ACCEPT
+    ";
+    trustedInterfaces = ["br+"];
+  };
 }
