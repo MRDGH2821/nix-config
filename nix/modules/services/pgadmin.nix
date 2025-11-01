ff951b6f330cdc7477603aa36332dcfc1847b100
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-11-02 09:55:11 +1100

    feat(pgadmin): :sparkles: add pgadmin local secrets and configuration

--- nix/modules/services/pgadmin.nix
+++ nix/modules/services/pgadmin.nix
@@ -4,6 +4,8 @@
     openFirewall = true;
     settings = {
       SERVER_MODE = true;
+      AUTHENTICATION_SOURCES = ["oauth2" "internal"];
+      OAUTH2_AUTO_CREATE_USER = true;
     };
     initialEmail = config.networking.email;
     initialPasswordFile = config.sops.secrets.dummyPassword.path;
@@ -15,5 +17,11 @@
       passwordFile = config.sops.secrets.smtpPassword.path;
       sender = config.networking.smtp.email;
     };
+  };
+  environment.etc."pgadmin/config_local.py" = {
+    source = config.sops.secrets.pgadmin.path;
+    mode = "0640";
+    owner = "pgadmin";
+    group = "pgadmin";
   };
 }
