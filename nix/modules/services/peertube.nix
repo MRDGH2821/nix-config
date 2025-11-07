e345403cdd601b1aec2007801ced3b435afcde66
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-11-07 22:48:55 +1100

    refactor(peertube): :recycle: comment out unused directory configurations and update SMTP password placeholder

--- nix/modules/services/peertube.nix
+++ nix/modules/services/peertube.nix
@@ -1,6 +1,30 @@
 {config, ...}: let
-  peertube-dir = "${config.persistent_storage}/peertube/storage";
+  # peertube-dir = "${config.persistent_storage}/peertube/storage";
 in {
+  # Create necessary directories for peertube
+  # systemd.tmpfiles.rules = [
+  #   "d ${peertube-dir} 0755 peertube peertube -"
+  #   "d ${peertube-dir}/tmp 0755 peertube peertube -"
+  #   "d ${peertube-dir}/tmp-persistent 0755 peertube peertube -"
+  #   "d ${peertube-dir}/bin 0755 peertube peertube -"
+  #   "d ${peertube-dir}/avatars 0755 peertube peertube -"
+  #   "d ${peertube-dir}/web-videos 0755 peertube peertube -"
+  #   "d ${peertube-dir}/streaming-playlists 0755 peertube peertube -"
+  #   "d ${peertube-dir}/original-video-files 0755 peertube peertube -"
+  #   "d ${peertube-dir}/redundancy 0755 peertube peertube -"
+  #   "d ${peertube-dir}/logs 0755 peertube peertube -"
+  #   "d ${peertube-dir}/previews 0755 peertube peertube -"
+  #   "d ${peertube-dir}/thumbnails 0755 peertube peertube -"
+  #   "d ${peertube-dir}/storyboards 0755 peertube peertube -"
+  #   "d ${peertube-dir}/torrents 0755 peertube peertube -"
+  #   "d ${peertube-dir}/captions 0755 peertube peertube -"
+  #   "d ${peertube-dir}/cache 0755 peertube peertube -"
+  #   "d ${peertube-dir}/plugins 0755 peertube peertube -"
+  #   "d ${peertube-dir}/well-known 0755 peertube peertube -"
+  #   "d ${peertube-dir}/uploads 0755 peertube peertube -"
+  #   "d ${peertube-dir}/client-overrides 0755 peertube peertube -"
+  # ];
+
   sops.templates."peertube.env" = {
     content = ''
       NODE_ENV=production
@@ -20,39 +44,39 @@ in {
         hostname = config.networking.smtp.host;
         port = config.networking.smtp.port;
         username = config.networking.smtp.email;
-        password = config.sops.placeholder.smtpPassword;
+        password = config.sops.placeholder.peertubeSmtpPassword;
         tls = true;
         from_address = config.networking.smtp.email;
       };
-      storage = {
-        tmp = "${peertube-dir}/tmp/";
-        tmp_persistent = "${peertube-dir}/tmp-persistent/"; # As tmp but the directory is not cleaned up between PeerTube restarts
-        bin = "${peertube-dir}/bin/";
-        avatars = "${peertube-dir}/avatars/";
-        web_videos = "${peertube-dir}/web-videos/";
-        streaming_playlists = "${peertube-dir}/streaming-playlists/";
-        original_video_files = "${peertube-dir}/original-video-files/";
-        redundancy = "${peertube-dir}/redundancy/";
-        logs = "${peertube-dir}/logs/";
-        previews = "${peertube-dir}/previews/";
-        thumbnails = "${peertube-dir}/thumbnails/";
-        storyboards = "${peertube-dir}/storyboards/";
-        torrents = "${peertube-dir}/torrents/";
-        captions = "${peertube-dir}/captions/";
-        cache = "${peertube-dir}/cache/";
-        plugins = "${peertube-dir}/plugins/";
-        well_known = "${peertube-dir}/well-known/"; # Various admin/user uploads that are not suitable for the folders above
-        uploads = "${peertube-dir}/uploads/"; # Overridable client files in client/dist/assets/images:
-        # - default-avatar-account-48x48.png
-        # - default-avatar-account.png
-        # - default-avatar-video-channel-48x48.png
-        # - default-avatar-video-channel.png
-        # - default-playlist.jpg
-        # Could contain for example "assets/images/default-playlist.jpg"
-        # If the file exists, peertube will serve it
-        # If not, peertube will fallback to the default file
-        client_overrides = "${peertube-dir}/client-overrides/";
-      };
+      # storage = {
+      #   tmp = "${peertube-dir}/tmp/";
+      #   tmp_persistent = "${peertube-dir}/tmp-persistent/"; # As tmp but the directory is not cleaned up between PeerTube restarts
+      #   bin = "${peertube-dir}/bin/";
+      #   avatars = "${peertube-dir}/avatars/";
+      #   web_videos = "${peertube-dir}/web-videos/";
+      #   streaming_playlists = "${peertube-dir}/streaming-playlists/";
+      #   original_video_files = "${peertube-dir}/original-video-files/";
+      #   redundancy = "${peertube-dir}/redundancy/";
+      #   logs = "${peertube-dir}/logs/";
+      #   previews = "${peertube-dir}/previews/";
+      #   thumbnails = "${peertube-dir}/thumbnails/";
+      #   storyboards = "${peertube-dir}/storyboards/";
+      #   torrents = "${peertube-dir}/torrents/";
+      #   captions = "${peertube-dir}/captions/";
+      #   cache = "${peertube-dir}/cache/";
+      #   plugins = "${peertube-dir}/plugins/";
+      #   well_known = "${peertube-dir}/well-known/"; # Various admin/user uploads that are not suitable for the folders above
+      #   uploads = "${peertube-dir}/uploads/"; # Overridable client files in client/dist/assets/images:
+      #   # - default-avatar-account-48x48.png
+      #   # - default-avatar-account.png
+      #   # - default-avatar-video-channel-48x48.png
+      #   # - default-avatar-video-channel.png
+      #   # - default-playlist.jpg
+      #   # Could contain for example "assets/images/default-playlist.jpg"
+      #   # If the file exists, peertube will serve it
+      #   # If not, peertube will fallback to the default file
+      #   client_overrides = "${peertube-dir}/client-overrides/";
+      # };
     };
     smtp.passwordFile = config.sops.secrets.smtpPassword.path;
     serviceEnvironmentFile = config.sops.templates."peertube.env".path;
