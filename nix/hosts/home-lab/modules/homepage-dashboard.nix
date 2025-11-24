4edd20ad1577d5322868d27d0bbc36627e3f036a
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-11-24 17:15:25 +1100

    feat(homepage-dashboard): :sparkles: enhance dashboard configuration with domain builder and additional widgets

--- nix/hosts/home-lab/modules/homepage-dashboard.nix
+++ nix/hosts/home-lab/modules/homepage-dashboard.nix
@@ -1,4 +1,6 @@
-{config, ...}: {
+{config, ...}: let
+  dmb = import ../../../mylib/domain-builder.nix {inherit config;};
+in {
   services.homepage-dashboard = {
     enable = true;
     settings = {
@@ -12,5 +14,49 @@
     openFirewall = true;
     listenPort = 7000;
     allowedHosts = config.networking.baseDomain;
+    widgets = [
+      {
+        datetime = {
+          format = {
+            dateStyle = "long";
+            timeStyle = "long";
+          };
+        };
+      }
+      {
+        resources = {
+          cpu = true;
+          disk = "/";
+          memory = true;
+        };
+      }
+
+      {
+        search = {
+          provider = "duckduckgo";
+          target = "_blank";
+        };
+      }
+    ];
+    services = [
+      {
+        Tools = [
+          {
+            "Omni Tools" = {
+              description = "Boost your productivity with OmniTools, the ultimate toolkit for getting things done quickly! Access thousands of user-friendly utilities for editing images, text, lists, and data, all directly from your browser.";
+              href = dmb.mkUrl "omni-tools" true;
+              icon = "sh-OmniTools";
+            };
+          }
+          {
+            "Bento PDF" = {
+              description = "The PDF Toolkit built for privacy.";
+              icon = "sh-BentoPDF";
+              href = dmb.mkUrl "pdf" true;
+            };
+          }
+        ];
+      }
+    ];
   };
 }
