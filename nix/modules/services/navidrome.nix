9f7dd83d17e681012da76ff11da3bf74273be7e7
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-11-26 10:44:53 +1100

    refactor(services): :wrench: mount rclone folders in navidrome

--- nix/modules/services/navidrome.nix
+++ nix/modules/services/navidrome.nix
@@ -1,9 +1,28 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  mylib = import ../../mylib/rclone-mounts.nix {inherit pkgs lib config;};
+  rclone-navidrome = "/mnt/rclone/navidrome";
+  music-folder = mylib.rcloneMount {
+    remoteName = "pcloud-personal";
+    folderName = "Music";
+    mountPoint = "${rclone-navidrome}/Music";
+  };
+  kirtan-folder = mylib.rcloneMount {
+    remoteName = "pcloud-personal";
+    folderName = "Kirtans";
+    mountPoint = "${rclone-navidrome}/Kirtans";
+  };
+in {
+  imports = [music-folder kirtan-folder];
   services.navidrome = {
     enable = true;
     openFirewall = true;
     settings = {
-      MusicFolder = "${config.persistent_storage}/Music/";
+      MusicFolder = rclone-navidrome;
       BaseUrl = "https://navidrome.${config.networking.baseDomain}";
       ReverseProxyUserHeader = "X-authentik-username";
     };
