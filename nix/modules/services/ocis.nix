cf3a79748c4f5d051b542254344d9a001e3c5f4f
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-01-21 11:43:37 +1100

    fix(service): allow unfree ocis

--- nix/modules/services/ocis.nix
+++ nix/modules/services/ocis.nix
@@ -1,4 +1,12 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "ocis_5-bin"
+    ];
   services.ocis = {
     enable = true;
     environmentFile = config.sops.secrets.ocis.path;
