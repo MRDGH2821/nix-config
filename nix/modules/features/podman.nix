9ba525797c4f30da950b74c763237380bda4a0c2
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-01-23 17:36:11 +1100

    refactor: migrate to podman
    Signed-off-by: MRDGH2821 <ask.mrdgh2821@outlook.com>

---
+++ nix/modules/features/podman.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    podman
+    podman-compose
+    compose2nix
+  ];
+  virtualisation.podman = {
+    enable = true;
+    autoPrune.enable = true;
+    dockerCompat = true;
+    networkSocket = {
+      enable = true;
+    };
+    defaultNetwork.settings.dns_enabled = true;
+  };
+}
