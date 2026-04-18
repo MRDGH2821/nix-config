5f3e770ed6c1ac308070f0296df2e9e2224e247c
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-04-18 13:57:29 +1000

    fix: fix path

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: let
-  autoImportLib = import ../../mylib/auto-import.nix {inherit lib;};
+  autoImportLib = import ../mylib/auto-import.nix {inherit lib;};
 in {
   imports =
     autoImportLib.autoImportModules ./.
