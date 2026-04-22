77f021d2cab7353252754fedc9e678539ecf16ab
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-04-23 09:27:13 +1000

    fix(services): allow open-webui unfree package

--- nix/system-modules/services/open-webui.nix
+++ nix/system-modules/services/open-webui.nix
@@ -1,7 +1,11 @@
-{
+{lib, ...}: {
   services.open-webui = {
     enable = true;
     openFirewall = true;
     port = 8181;
   };
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "open-webui"
+    ];
 }
