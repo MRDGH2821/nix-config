6b8d3eba02f3a59fd38546db68f09d74bfc2e76f
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-05-16 21:42:50 +1000

    fix(services): add nginx config

--- nix/system-modules/services/bentopdf.nix
+++ nix/system-modules/services/bentopdf.nix
@@ -2,5 +2,14 @@
   services.bentopdf = {
     enable = true;
     domain = "pdf.${config.networking.baseDomain}";
+    nginx.enable = true;
+  };
+  services.nginx.virtualHosts."${config.services.bentopdf.domain}" = {
+    listen = [
+      {
+        addr = "127.0.0.1";
+        port = 8090;
+      }
+    ];
   };
 }
