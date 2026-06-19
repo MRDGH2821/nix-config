f6ab2cc5ebef9b4b685de5f9073215493e84490f
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-06-20 06:51:25 +1000

    fix(services): regenrate podman compose.nix file

--- nix/system-modules/container-services/omniroute/podman-compose.nix
+++ nix/system-modules/container-services/omniroute/podman-compose.nix
@@ -26,7 +26,7 @@
 
   # Containers
   virtualisation.oci-containers.containers."omniroute-web" = {
-    image = "ghcr.io/diegosouzapw/omniroute:3.8.28-web";
+    image = "ghcr.io/diegosouzapw/omniroute:3.8.29-web";
     volumes = [
       "omniroute_omniroute:/app/data:rw"
     ];
