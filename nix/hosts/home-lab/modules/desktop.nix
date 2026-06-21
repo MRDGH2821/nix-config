be580269d1f427f11fca13b8d917309fbaadd566
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-06-21 16:39:38 +1000

    feat: add folder mount

--- nix/hosts/home-lab/modules/desktop.nix
+++ nix/hosts/home-lab/modules/desktop.nix
@@ -1,4 +1,22 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  mylib = import ../../mylib/rclone-mounts.nix {inherit pkgs lib config;};
+  rclone-kpxc = "/mnt/rclone/kpxc";
+  mount-options = "rw";
+  keepassxc-folder = mylib.rcloneMount {
+    remoteName = "pcloud-personal";
+    folderName = "Keepass";
+    mountPoint = "${rclone-kpxc}/Keepass";
+    options = mount-options;
+  };
+in {
+  imports = [
+    keepassxc-folder
+  ];
   services.desktopManager.plasma6.enable = true;
 
   services.openssh.enable = true;
@@ -7,6 +25,7 @@
   environment.systemPackages = with pkgs; [
     waypipe
     zed-editor
+    keepassxc
   ];
 
   users.users.mr-nix.extraGroups = [
