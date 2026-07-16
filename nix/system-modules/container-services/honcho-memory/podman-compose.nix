d068b7cabc833aa125c0bfe1bacf8137663a9399
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-07-17 06:11:03 +1000

    chore: update repo

--- nix/system-modules/container-services/honcho-memory/podman-compose.nix
+++ nix/system-modules/container-services/honcho-memory/podman-compose.nix
@@ -26,7 +26,7 @@
 
   # Containers
   virtualisation.oci-containers.containers."honcho-memory-api" = {
-    image = "ghcr.io/plastic-labs/honcho:v3.0.11";
+    image = "ghcr.io/plastic-labs/honcho:v3.0.12";
     log-driver = "journald";
     extraOptions = [
       "--network=host"
@@ -44,7 +44,7 @@
     ];
   };
   virtualisation.oci-containers.containers."honcho-memory-deriver" = {
-    image = "ghcr.io/plastic-labs/honcho:v3.0.11";
+    image = "ghcr.io/plastic-labs/honcho:v3.0.12";
     dependsOn = [
       "honcho-memory-api"
     ];
@@ -66,7 +66,7 @@
     ];
   };
   virtualisation.oci-containers.containers."honcho-memory-migrate" = {
-    image = "ghcr.io/plastic-labs/honcho:v3.0.11";
+    image = "ghcr.io/plastic-labs/honcho:v3.0.12";
     log-driver = "journald";
     extraOptions = [
       "--entrypoint=[\"/app/.venv/bin/alembic\", \"upgrade\", \"head\"]"
