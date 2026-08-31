5ec1e07d352c6b1cf9102ae9bd83822a5ac03c87
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-09-01 08:57:59 +1000

    chore: make a minimal config

--- nix/hosts/fw16/users/mr-fw16.nix
+++ nix/hosts/fw16/users/mr-fw16.nix
@@ -1,22 +1,25 @@
-{inputs, ...}: {
+{pkgs, ...}: {
   home = {
     homeDirectory = "/home/mr-fw16";
+    packages = with pkgs; [
+      llm-pkgs.claude-desktop
+    ];
     # Real Fedora login on the Framework 16 (not "mr-nix").
     username = "mr-fw16";
   };
-  imports = [
-    inputs.self.homeModules.common
-    inputs.self.homeModules.mr-nix
-  ];
-  mine = {
-    git = {
-      # Public GPG key id (not a secret); value from chezmoi dot_config/git/config -> [user].signingkey.
-      signingKey = "1915CBA05A598D01"; # pragma: allowlist secret
-      userEmail = "ask.mrdgh2821@outlook.com";
-      userName = "MRDGH2821";
-    };
-    gui.enable = true;
-  };
+  # imports = [
+  #   # inputs.self.homeModules.common
+  #   # inputs.self.homeModules.mr-nix
+  # ];
+  # mine = {
+  #   git = {
+  #     # Public GPG key id (not a secret); value from chezmoi dot_config/git/config -> [user].signingkey.
+  #     signingKey = "1915CBA05A598D01"; # pragma: allowlist secret
+  #     userEmail = "ask.mrdgh2821@outlook.com";
+  #     userName = "MRDGH2821";
+  #   };
+  #   gui.enable = true;
+  # };
   # Home Manager running on Fedora, not NixOS.
   targets.genericLinux.enable = true;
 }
